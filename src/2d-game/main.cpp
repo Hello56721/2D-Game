126e70621593f6debdb8679ed77652f6894d9d7b
Hello56721 <Hello56721@github.com>
2020-10-16 16:31:31 -0600

    Created a GLFW window

--- src/2d-game/main.cpp
+++ src/2d-game/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
 int main() {
-    std::cout << "Hello, this is a test!" << std::endl;
+    if (!glfwInit()) {
+        std::cout << "ERROR: Could not initialize GLFW." << std::endl;
+        return -1;
+    }
+    // Make GLFW use OpenGL 3.3 core or higher
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "2D Game", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "ERROR: Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    // Load the OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "ERROR: Failed to load OpenGL function pointers" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
     return 0;
 }
