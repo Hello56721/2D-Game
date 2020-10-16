c0d83a947e6b9dbe404c09432f2bae425305ebee
Hello56721 <Hello56721@github.com>
2020-10-16 17:05:02 -0600

    Added Engine graphic's utils header

--- src/2d-game/main.cpp
+++ src/2d-game/main.cpp
@@ -12,7 +12,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
+
     GLFWwindow* window = glfwCreateWindow(800, 600, "2D Game", nullptr, nullptr);
     if (window == nullptr) {
         std::cout << "ERROR: Failed to create GLFW window." << std::endl;
@@ -20,22 +20,33 @@ int main() {
         return -1;
     }
     glfwMakeContextCurrent(window);
-    
+
     // Load the OpenGL function pointers
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "ERROR: Failed to load OpenGL function pointers" << std::endl;
         glfwTerminate();
         return -1;
     }
-    
+
+    float vertices = {
+         0.5f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
+    };
+    unsigned int indices = {
+        0, 1, 2,
+        0, 3, 2
+    };
+
     while (!glfwWindowShouldClose(window)) {
         glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+
     glfwTerminate();
     return 0;
 }
