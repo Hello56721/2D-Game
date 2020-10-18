d92307469aa90b39d82bb09bcf4a4f37e89c4262
Hello56721 <Hello56721@github.com>
2020-10-17 18:59:25 -0600

    Switched to core profile

--- src/2d-game/main.cpp
+++ src/2d-game/main.cpp
@@ -9,9 +9,9 @@ int main() {
         return -1;
     }
     // Make GLFW use OpenGL 3.3 core or higher
-    /*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
     // Hide the GLFW window on startup
     glfwWindowHint(GLFW_VISIBLE, false);
