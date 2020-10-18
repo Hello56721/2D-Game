3dd9baa7712870d43de92ab275343a16c82f0b2a
Hello56721 <Hello56721@github.com>
2020-10-17 18:41:20 -0600

    Drawn the first rectangle

--- src/2d-game/main.cpp
+++ src/2d-game/main.cpp
@@ -9,9 +9,9 @@ int main() {
         return -1;
     }
     // Make GLFW use OpenGL 3.3 core or higher
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    /*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
     
     // Hide the GLFW window on startup
     glfwWindowHint(GLFW_VISIBLE, false);
@@ -46,12 +46,30 @@ int main() {
         0, 3, 2
     };
     
-    glCall(glBindBuffer(GL_UNSIGNED_INT, 0));
+    unsigned int vertexArray;
+    glCall(glGenVertexArrays(1, &vertexArray));
+    glCall(glBindVertexArray(vertexArray));
+    
+    unsigned int vertexBuffer;
+    glCall(glGenBuffers(1, &vertexBuffer));
+    glCall(glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer));
+    glCall(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
+    
+    unsigned int elemenBuffer;
+    glCall(glGenBuffers(1, &elemenBuffer));
+    glCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elemenBuffer));
+    glCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW));
+    
+    glCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0));
+    glCall(glEnableVertexAttribArray(0));
     
     glfwShowWindow(window);
     while (!glfwWindowShouldClose(window)) {
         glCall(glClearColor(1.0f, 0.5f, 0.0f, 1.0f));
         glCall(glClear(GL_COLOR_BUFFER_BIT));
+
+        glCall(glBindVertexArray(vertexArray));
+        glCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         glfwSwapBuffers(window);
         glfwPollEvents();
