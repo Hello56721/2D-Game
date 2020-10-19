59300bf840fca5a9209a8e1e28f130264c58be3a
Hello56721 <Hello56721@github.com>
2020-10-19 11:28:42 -0600

    Started on the Mesh class

---
+++ include/EngineGraphics/Mesh.hpp
@@ -0,0 +1,21 @@
+#ifndef INCLUDED_EngineGraphics_Mesh_hpp
+#define INCLUDED_EngineGraphics_Mesh_hpp
+
+#pragma once
+
+#include <vector>
+
+namespace EngineGraphics {
+    class Mesh {
+    private:
+        // the handles to OpenGL objects
+        unsigned int vertexArray;
+        unsigned int vertexBuffer;
+        unsigned int elementBuffer;
+        
+        // vertices
+        
+    };
+}
+
+#endif
