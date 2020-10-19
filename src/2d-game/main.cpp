360842c26a23c33afdd85becb388eeeac501f68e
Hello56721 <Hello56721@github.com>
2020-10-19 14:07:58 -0600

    Removed commented out code

--- src/2d-game/main.cpp
+++ src/2d-game/main.cpp
@@ -37,17 +37,6 @@ int main() {
         glfwTerminate();
         return -1;
     }
-
-    /*float vertices[] = {
-         0.5f,  0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f
-    };
-    unsigned int indices[] = {
-        0, 1, 2,
-        0, 3, 2
-    };*/
     
     std::vector<Vertex> vertices = {
         {{ 0.5f,  0.5f, 0.0f}},
@@ -64,32 +53,12 @@ int main() {
     
     EngineGraphics::Shader shader("../shaders/basic/vertex.glsl", "../shaders/basic/fragment.glsl", "shader program");
     
-    /*unsigned int vertexArray;
-    glCall(glGenVertexArrays(1, &vertexArray));
-    glCall(glBindVertexArray(vertexArray));
-    
-    unsigned int vertexBuffer;
-    glCall(glGenBuffers(1, &vertexBuffer));
-    glCall(glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer));
-    glCall(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
-    
-    unsigned int elemenBuffer;
-    glCall(glGenBuffers(1, &elemenBuffer));
-    glCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elemenBuffer));
-    glCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW));
-    
-    glCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0));
-    glCall(glEnableVertexAttribArray(0));*/
-    
     glfwShowWindow(window);
     while (!glfwWindowShouldClose(window)) {
         glCall(glClearColor(1.0f, 0.5f, 0.0f, 1.0f));
         glCall(glClear(GL_COLOR_BUFFER_BIT));
         
         shader.use();
-
-        //glCall(glBindVertexArray(vertexArray));
-        //glCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
         mesh.render();
 
         glfwSwapBuffers(window);
