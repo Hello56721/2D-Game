5998ea5363482c8b27a44624637a0f494b58290e
Hello56721 <Hello56721@github.com>
2020-10-23 08:43:10 -0600

    Used the Display utility

--- src/2d-game/main.cpp
+++ src/2d-game/main.cpp
@@ -1,42 +1,13 @@
 #include <iostream>
 
 #include <EngineGraphics/Utils.hpp>
-#include <GLFW/glfw3.h>
+#include "Display.hpp"
 
 #include <EngineGraphics/Shader.hpp>
 #include <EngineGraphics/Mesh.hpp>
 
 int main() {
-    if (!glfwInit()) {
-        std::cout << "ERROR: Could not initialize GLFW." << std::endl;
-        return -1;
-    }
-    // Make GLFW use OpenGL 3.3 core or higher
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
-    // Hide the GLFW window on startup
-    glfwWindowHint(GLFW_VISIBLE, false);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "2D Game", nullptr, nullptr);
-    if (window == nullptr) {
-        std::cout << "ERROR: Failed to create GLFW window." << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    
-    // Put the window in the center of the screen
-    const GLFWvidmode* videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-    glfwSetWindowPos(window, (videoMode->width - 800) / 2, (videoMode->height - 600) / 2);
-
-    // Load the OpenGL function pointers
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "ERROR: Failed to load OpenGL function pointers" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+    Display::create(false);
     
     std::vector<Vertex> vertices = {
         {{ 0.5f,  0.5f, 0.0f}},
@@ -53,16 +24,17 @@ int main() {
     
     EngineGraphics::Shader* shader = new EngineGraphics::Shader("../shaders/basic/vertex.glsl", "../shaders/basic/fragment.glsl", "shader program");
     
-    glfwShowWindow(window);
-    while (!glfwWindowShouldClose(window)) {
+    glfwShowWindow(Display::window);
+    while (Display::isOpen()) {
         glCall(glClearColor(1.0f, 0.5f, 0.0f, 1.0f));
         glCall(glClear(GL_COLOR_BUFFER_BIT));
         
         shader->use();
         mesh->render();
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        //glfwSwapBuffers(window);
+        //glfwPollEvents();
+        Display::update();
     }
     
     delete mesh;
